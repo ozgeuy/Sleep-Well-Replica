a17ac22b05e14c562c36bcbd86513dc75721eaf2
Merve Şensoy <merveesensoy@hotmail.com>
2021-08-19 12:20:27 +0300

    Level Texts Controller(TextController) Script Added

--- Sleep Well Replica/Assets/Scripts/LevelManager.cs
+++ Sleep Well Replica/Assets/Scripts/LevelManager.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    public static LevelManager Instance;
+
+    public int Level = 1;
+
+    public TextController TextController;
+
     [SerializeField] private GameObject[] levels;
+
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instance = this; 
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
---
+++ Sleep Well Replica/Assets/TextController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextController : MonoBehaviour
+{
+
+
+    [SerializeField] private TMP_Text levelText;
+    [SerializeField] private TMP_Text missionText;
+
+    [SerializeField] private string[] levelMissions;
+
+    private int Iterator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+    }
+
+    public void ResetTexts()
+    {
+        Iterator = (LevelManager.Instance.Level - 1);
+        levelText.text = "LEVEL " + LevelManager.Instance.Level;
+        missionText.text = levelMissions[Iterator];
+
+    }
+}
